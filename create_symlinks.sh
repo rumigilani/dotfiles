be4af863f2c97a9465694d371fb77c65bfb6ae8f
rumigilani <rumijgilani@gmail.com>
2017-05-28 15:55:16 -0700

    added polybar

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -7,3 +7,5 @@ ln -s ~/dotfiles/.config/i3/config ~/.config/i3
 ln -s ~/dotfiles/.config/compton.conf ~/.config
 
 ln -s ~/dotfiles/.zshrc ~
+
+ln -s ~/dotfiles/.config/polybar/config ~/.config/polybar
