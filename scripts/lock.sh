1c4a65dd28c197c1ca3d8bf4621462bfdeb01540
rumigilani <rumijgilani@gmail.com>
2017-05-29 04:36:47 -0700

    fixed images directory

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ICON=$HOME/Pictures/Icons/icon.png
+ICON=$HOME/dotfiles/images/lock-icon.png
 TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
